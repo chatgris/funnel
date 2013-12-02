ef5fbd45d8524fe86e256f0ec11475449dfca7a7
chatgris <chatgri@gmail.com>
2013-12-02 13:39:53 +0900

    Add `/ohai` endpoint
    Signed-off-by: chatgris <jboyer@af83.com>

---
+++ test/routers/ohai_router_test.exs
@@ -0,0 +1,23 @@
+defmodule OhaiRouterTest do
+  use Funnel.TestCase
+  use Dynamo.HTTP.Case
+
+  @endpoint OhaiRouter
+
+  test "returns 201" do
+    conn = post("/")
+    assert conn.status == 201
+  end
+
+  test "returns json" do
+    conn = post("/")
+    assert conn.resp_headers["Content-Type"] == "application/json"
+  end
+
+  test "returns a token" do
+    conn = post("/")
+    {:ok, body} = JSEX.decode conn.resp_body
+    assert size(body["token"]) == 36
+  end
+
+end
