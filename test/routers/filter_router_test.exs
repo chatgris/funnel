01d42bb1b43c505cef34c1678cf5fc44dcd46b04
chatgris <chatgri@gmail.com>
2013-12-10 19:34:59 +0900

    Allow to update a filter
    Signed-off-by: chatgris <jboyer@af83.com>

--- test/routers/filter_router_test.exs
+++ test/routers/filter_router_test.exs
@@ -9,6 +9,11 @@ defmodule FilterRouterTest do
     assert conn.status == 400
   end
 
+  test "returns 400 when an empty body is given on put" do
+    conn = put("/uuid")
+    assert conn.status == 400
+  end
+
   test "returns json when an empty body is given" do
     conn = post("/")
     assert conn.resp_headers["Content-Type"] == "application/json"
@@ -19,6 +24,19 @@ defmodule FilterRouterTest do
     conn = conn(:POST, "/", body)
     conn = post(conn, "/")
     assert conn.status == 201
+  end
+
+  test "returns 200" do
+    body = '{"query" : {"term" : {"field1" : "value1"}}}'
+    conn = conn(:POST, "/", body)
+    conn = post(conn, "/")
+    assert conn.status == 201
+    {:ok, body} = JSEX.decode conn.resp_body
+    uuid = body["query_id"]
+    body = '{"query" : {"term" : {"field1" : "value2"}}}'
+    conn = conn(:PUT, uuid, body)
+    conn = put(conn, uuid)
+    assert conn.status == 200
   end
 
   test "returns json" do
