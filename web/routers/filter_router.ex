701ac1882cd7c1e2d73f94cba082406b98690fe0
chatgris <chatgri@gmail.com>
2013-12-10 18:01:06 +0900

    Add Funnel.Es.percolate/1
    Signed-off-by: chatgris <jboyer@af83.com>

--- web/routers/filter_router.ex
+++ web/routers/filter_router.ex
@@ -1,15 +1,11 @@
 defmodule FilterRouter do
   use Dynamo.Router
 
-  prepare do
-    conn.fetch([:body])
-  end
+  prepare do: conn.fetch([:body])
 
   post "/" do
-    percolation =  Funnel.Es.put("/_percolator/funnel/message", conn.req_body)
-    {:ok, body} = JSEX.decode(percolation.body)
-    {:ok, response} = JSEX.encode([query_id: Funnel.Uuid.generate, body: body])
+    {status_code, response} = Funnel.Es.percolate conn.req_body
     conn = conn.put_resp_header "Content-Type", "application/json"
-    conn.resp percolation.status_code, response
+    conn.resp status_code, response
   end
 end
