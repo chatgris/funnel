4474a80851aa844be665a2c5f6cd36d856c1ad99
chatgris <jboyer@af83.com>
2013-12-10 19:34:59 +0900

    Allow to update a filter
    Signed-off-by: chatgris <jboyer@af83.com>

--- web/routers/filter_router.ex
+++ web/routers/filter_router.ex
@@ -1,10 +1,16 @@
 defmodule FilterRouter do
   use Dynamo.Router
 
-  prepare do: conn.fetch([:body])
+  prepare do: conn.fetch([:body, :params])
 
   post "/" do
     {status_code, response} = Funnel.Es.percolate conn.req_body
+    conn = conn.put_resp_header "Content-Type", "application/json"
+    conn.resp status_code, response
+  end
+
+  put ":uuid" do
+    {status_code, response} = Funnel.Es.percolate conn.params[:uuid], conn.req_body
     conn = conn.put_resp_header "Content-Type", "application/json"
     conn.resp status_code, response
   end
