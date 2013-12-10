cb5cae536b7a886fb98d733df00187cb5f44be8e
chatgris <chatgri@gmail.com>
2013-12-10 19:20:40 +0900

    Funnel.Es.percolate Allow to update a filter
    Signed-off-by: chatgris <jboyer@af83.com>

--- test/funnel/es_test.exs
+++ test/funnel/es_test.exs
@@ -9,6 +9,20 @@ defmodule EsTest do
     Funnel.Es.unpercolate(body["query_id"])
   end
 
+  test "update a query creation" do
+    body = '{"query" : {"term" : {"field1" : "value1"}}}'
+    {status, response} = Funnel.Es.percolate(body)
+    {:ok, body} = JSEX.decode response
+    uuid = body["query_id"]
+    assert status == 201
+
+    body = '{"query" : {"term" : {"field1" : "value2"}}}'
+    {status, response} = Funnel.Es.percolate(uuid, body)
+    {:ok, body} = JSEX.decode response
+    assert status == 200
+    Funnel.Es.unpercolate(body["query_id"])
+  end
+
   test "returns a body on query creation" do
     body = '{"query" : {"term" : {"field1" : "value1"}}}'
     {_, response} = Funnel.Es.percolate(body)
