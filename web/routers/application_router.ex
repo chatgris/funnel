7752b13241d7980c3b535d026e348e2b5c63b408
chatgris <chatgri@gmail.com>
2013-12-11 22:23:18 +0900

    Cleanup ApplicationRouter
    Signed-off-by: chatgris <jboyer@af83.com>

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -1,25 +1,12 @@
 defmodule ApplicationRouter do
   use Dynamo.Router
-
-  prepare do
-    # Pick which parts of the request you want to fetch
-    # You can comment the line below if you don't need
-    # any of them or move them to a forwarded router
-    conn.fetch([:cookies, :params, :body])
-  end
-
-  # It is common to break your Dynamo into many
-  # routers, forwarding the requests between them:
-  # forward "/posts", to: PostsRouter
+  filter JsonHeader
 
   forward "/status", to: StatusRouter
-
   forward "/ohai",   to: OhaiRouter
-
   forward "/filter", to: FilterRouter
 
   get "/" do
-    conn = conn.put_resp_header "Content-Type", "application/json"
     {:ok, payload} = JSEX.encode [message: "Welcome to funnel"]
     conn.resp 200, payload
   end
