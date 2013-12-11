0c501024ddaa3cb54f6bee0aaef9f440a9fb5b29
chatgris <jboyer@af83.com>
2013-12-11 12:35:19 +0900

    More attemp to get a req_body in tests
    Signed-off-by: chatgris <jboyer@af83.com>

--- test/routers/filter_router_test.exs
+++ test/routers/filter_router_test.exs
@@ -15,13 +15,17 @@ defmodule FilterRouterTest do
   end
 
   test "returns json when an empty body is given" do
-    conn = post("/")
+    body = '{"query" : {"term" : {"field1" : "value1"}}}'
+    conn = conn(:POST, "/", body)
+    conn = conn.put_req_header "Content-Type", "application/json"
+    conn = post(conn, "/")
     assert conn.resp_headers["Content-Type"] == "application/json"
   end
 
   test "returns 201" do
     body = '{"query" : {"term" : {"field1" : "value1"}}}'
     conn = conn(:POST, "/", body)
+    conn = conn.put_req_header "Content-Type", "application/json"
     conn = post(conn, "/")
     assert conn.status == 201
   end
@@ -29,19 +33,23 @@ defmodule FilterRouterTest do
   test "returns 200" do
     body = '{"query" : {"term" : {"field1" : "value1"}}}'
     conn = conn(:POST, "/", body)
+    conn = conn.put_req_header "Content-Type", "application/json"
     conn = post(conn, "/")
     assert conn.status == 201
     {:ok, body} = JSEX.decode conn.resp_body
     uuid = body["filter_id"]
     body = '{"query" : {"term" : {"field1" : "value2"}}}'
     conn = conn(:PUT, uuid, body)
+    conn = conn.put_req_header "Content-Type", "application/json"
     conn = put(conn, uuid)
     assert conn.status == 200
+    Funnel.Es.unpercolate(uuid)
   end
 
   test "returns json" do
     body = '{"query" : {"term" : {"field1" : "value1"}}}'
     conn = conn(:POST, "/", body)
+    conn = conn.put_req_header "Content-Type", "application/json"
     conn = post(conn, "/")
     assert conn.resp_headers["Content-Type"] == "application/json"
   end
@@ -49,6 +57,7 @@ defmodule FilterRouterTest do
   test "returns a filter_id" do
     body = '{"query" : {"term" : {"field1" : "value1"}}}'
     conn = conn(:POST, "/", body)
+    conn = conn.put_req_header "Content-Type", "application/json"
     conn = post(conn, "/")
     {:ok, body} = JSEX.decode conn.resp_body
     assert size(body["filter_id"]) == 36
