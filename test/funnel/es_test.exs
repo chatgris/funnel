9e187d41471ee25d99ecc54d5028850ed704538a
chatgris <chatgri@gmail.com>
2013-12-12 00:10:42 +0900

    Rename percolate to register
    Signed-off-by: chatgris <jboyer@af83.com>

--- test/funnel/es_test.exs
+++ test/funnel/es_test.exs
@@ -3,38 +3,38 @@ defmodule EsTest do
 
   test "returns a 201 on query creation" do
     body = '{"query" : {"term" : {"field1" : "value1"}}}'
-    {status, response} = Funnel.Es.percolate(body)
+    {status, response} = Funnel.Es.register(body)
     {:ok, body} = JSEX.decode response
     assert status == 201
-    Funnel.Es.unpercolate(body["filter_id"])
+    Funnel.Es.unregister(body["filter_id"])
   end
 
   test "update a query creation" do
     body = '{"query" : {"term" : {"field1" : "value1"}}}'
-    {status, response} = Funnel.Es.percolate(body)
+    {status, response} = Funnel.Es.register(body)
     {:ok, body} = JSEX.decode response
     uuid = body["filter_id"]
     assert status == 201
 
     body = '{"query" : {"term" : {"field1" : "value2"}}}'
-    {status, response} = Funnel.Es.percolate(uuid, body)
+    {status, response} = Funnel.Es.register(uuid, body)
     {:ok, body} = JSEX.decode response
     assert status == 200
-    Funnel.Es.unpercolate(body["filter_id"])
+    Funnel.Es.unregister(body["filter_id"])
   end
 
   test "returns a body on query creation" do
     body = '{"query" : {"term" : {"field1" : "value1"}}}'
-    {_, response} = Funnel.Es.percolate(body)
+    {_, response} = Funnel.Es.register(body)
     {:ok, body} = JSEX.decode response
     assert size(body["filter_id"]) == 36
-    Funnel.Es.unpercolate(body["filter_id"])
+    Funnel.Es.unregister(body["filter_id"])
   end
 
   test "returns a 400 on bad payload" do
-    {status, response} = Funnel.Es.percolate("")
+    {status, response} = Funnel.Es.register("")
     {:ok, body} = JSEX.decode response
     assert status == 400
-    Funnel.Es.unpercolate(body["filter_id"])
+    Funnel.Es.unregister(body["filter_id"])
   end
 end
