d206f2da4165ed573edcf824a0bd689ed049b092
chatgris <jboyer@af83.com>
2014-01-02 15:22:49 +0100

    FilterRouter nested in IndexRouter
    closes #1
    
    Signed-off-by: chatgris <jboyer@af83.com>

--- test/funnel/es_test.exs
+++ test/funnel/es_test.exs
@@ -5,48 +5,48 @@ defmodule EsTest do
 
   test "returns a 201 on query creation" do
     body = '{"query" : {"term" : {"field1" : "value1"}}}'
-    {status, response} = Funnel.Es.register("token", body)
+    {status, response} = Funnel.Es.register("funnel", "token", body)
     {:ok, body} = JSEX.decode response
     assert status == 201
-    Funnel.Es.unregister("token", body["filter_id"])
+    Funnel.Es.unregister("funnel", "token", body["filter_id"])
   end
 
   test "update a query creation" do
     body = '{"query" : {"term" : {"field1" : "value1"}}}'
-    {status, response} = Funnel.Es.register("token", body)
+    {status, response} = Funnel.Es.register("funnel", "token", body)
     {:ok, body} = JSEX.decode response
     uuid = body["filter_id"]
     assert status == 201
 
     body = '{"query" : {"term" : {"field1" : "value2"}}}'
-    {status, response} = Funnel.Es.register("token", uuid, body)
+    {status, response} = Funnel.Es.register("funnel", "token", uuid, body)
     {:ok, body} = JSEX.decode response
     assert status == 200
-    Funnel.Es.unregister("token", body["filter_id"])
+    Funnel.Es.unregister("funnel", "token", body["filter_id"])
   end
 
   test "returns a body on query creation" do
     body = '{"query" : {"term" : {"field1" : "value1"}}}'
-    {_, response} = Funnel.Es.register("token", body)
+    {_, response} = Funnel.Es.register("funnel", "token", body)
     {:ok, body} = JSEX.decode response
     assert size(body["filter_id"]) == 32
-    Funnel.Es.unregister("token", body["filter_id"])
+    Funnel.Es.unregister("funnel", "token", body["filter_id"])
   end
 
   test "returns a 400 on bad payload" do
-    {status, response} = Funnel.Es.register("token", "")
+    {status, response} = Funnel.Es.register("funnel", "token", "")
     assert status == 400
   end
 
   test "returns queries from percolator" do
     body = '{"query" : {"field" : {"message" : "elasticsearch"}}}'
     message = '{"doc" : {"message" : "this new elasticsearch percolator feature is nice, borat style"}}'
-    {_, response} = Funnel.Es.register("token", body)
+    {_, response} = Funnel.Es.register("funnel", "token", body)
     {:ok, body} = JSEX.decode response
     uuid = body["filter_id"]
     Funnel.Es.refresh
     assert Funnel.Es.percolate(message) == ["token-#{uuid}"]
-    Funnel.Es.unregister("token", uuid)
+    Funnel.Es.unregister("funnel", "token", uuid)
   end
 
   test "returns empty from percolator on non match" do
--- web/routers/index_router.ex
+++ web/routers/index_router.ex
@@ -3,6 +3,8 @@ defmodule IndexRouter do
   filter JsonHeader
   filter TokenFilter
 
+  forward "/:index_id", to: FilterRouter
+
   post "/" do
     {status_code, response} = Funnel.Es.create conn.req_body
     conn.resp status_code, response
