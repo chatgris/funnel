ee2d1b2d38d02016d0d22ce149334be0646a8c96
chatgris <chatgri@gmail.com>
2014-01-07 11:36:22 +0100

    Less code in Funnel.Percolator
    Thanks to ExActor
    
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/funnel/percolator.ex
+++ lib/funnel/percolator.ex
@@ -1,19 +1,7 @@
 defmodule Funnel.Percolator do
-  use GenServer.Behaviour
-
-  def start_link do
-    :gen_server.start_link({:local, :percolator}, __MODULE__, nil, [])
-  end
+  use ExActor, export: :percolator
 
   def percolate(body) do
-    :gen_server.cast :percolator, {:percolate, body}
-  end
-
-  def init do
-    {:ok, nil}
-  end
-
-  def handle_cast({:percolate, body}, nil) do
     Funnel.Es.percolate(body)
       |> Enum.each(fn(match)-> notify(match, body) end)
     { :noreply, nil}
