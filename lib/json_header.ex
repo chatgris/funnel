bd45ac66aead157f03392aff1c1e91d41da0639c
chatgris <chatgri@gmail.com>
2014-01-14 10:43:30 +0100

    [Doc] JsonHeader
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/json_header.ex
+++ lib/json_header.ex
@@ -1,4 +1,7 @@
 defmodule JsonHeader do
+  @moduledoc """
+  This module set the response `Content-Type` Header to `application/json`,
+  """
   def prepare(conn) do
     conn.put_resp_header "Content-Type", "application/json"
   end
