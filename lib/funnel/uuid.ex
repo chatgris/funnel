1da05fc5e118cde1c3a176d9d2e2cbc999889638
chatgris <jboyer@af83.com>
2014-01-14 10:49:10 +0100

    [Doc] Funnel.Uuid
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/funnel/uuid.ex
+++ lib/funnel/uuid.ex
@@ -1,9 +1,13 @@
 defmodule Funnel.Uuid do
   @module_doc """
 
-  Wrapper around uuid
+  Wrapper around uuid library.
   """
 
+  @doc """
+
+  Returns a sweet uuid.
+  """
   def generate do
     String.replace(to_string(:uuid.to_string(:uuid.v4())), "-", "")
   end
