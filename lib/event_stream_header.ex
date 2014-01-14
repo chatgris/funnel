57fa02c34292f95956294bbfcc55d621a581ac15
chatgris <chatgri@gmail.com>
2014-01-14 10:42:32 +0100

    [Doc] EventStreamHeader
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/event_stream_header.ex
+++ lib/event_stream_header.ex
@@ -1,4 +1,8 @@
 defmodule EventStreamHeader do
+  @moduledoc """
+  This module set the response `Content-Type` Header to `text/events-stream`,
+  the ServerSent Events header.
+  """
   def prepare(conn) do
     conn.put_resp_header "Content-Type", "text/events-stream"
   end
