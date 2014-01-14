8f8bd7cb92b1d1f978cbf57805f7e24529765453
chatgris <chatgri@gmail.com>
2014-01-14 10:46:41 +0100

    [Doc] TokenFilter
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/token_filter.ex
+++ lib/token_filter.ex
@@ -1,4 +1,8 @@
 defmodule TokenFilter do
+  @moduledoc """
+  This module checks the presence of the `:token` params. Otherwise, it will
+  halt the connection with `400` http return code
+  """
   use Dynamo.Router
 
   def prepare(conn) do
