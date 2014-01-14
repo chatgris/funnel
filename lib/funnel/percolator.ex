113449a738895eb06e04d1a2fd7919aeb558253e
chatgris <chatgri@gmail.com>
2014-01-14 10:54:56 +0100

    [Doc] Funnel.Percolator
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/funnel/percolator.ex
+++ lib/funnel/percolator.ex
@@ -1,18 +1,41 @@
 defmodule Funnel.Percolator do
+  @moduledoc """
+  `Funnel.Percolator` is meant to receive documents, send those documents to the
+  Elasticsearch percolation, then, notify each User.
+  """
+
   use GenServer.Behaviour
 
+  @doc """
+
+  Start a new `Funnel.Percolator` actor.
+  """
   def start_link do
     :gen_server.start_link({:local, :percolator}, __MODULE__, nil, [])
   end
 
+  @doc """
+
+  Wrapper around `GenServer`. Send a document to Elasticsearch's percolator.
+
+  * `body`     - Document in json
+  """
   def percolate(body) do
     :gen_server.cast :percolator, {:percolate, body}
   end
 
+  @doc """
+
+  Default values of `Funnel.Percolator`.
+  """
   def init do
     {:ok, nil}
   end
 
+  @doc """
+
+  Percolates and notify on each match.
+  """
   def handle_cast({:percolate, body}, nil) do
     Funnel.Es.percolate(body)
       |> Enum.each(fn(match)-> notify(match, body) end)
