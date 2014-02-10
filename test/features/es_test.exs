7d0859025e1ceb63fd1b8b83a1cef40cc2c10340
chatgris <jboyer@af83.com>
2014-02-10 09:58:15 +0100

    Remove useless test
    Signed-off-by: chatgris <jboyer@af83.com>

--- test/features/es_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule Funnel.EsTest do
-  use Funnel.TestCase, async: true
-
-  test "Returns Elasticsearc status" do
-    response = Funnel.Es.get("/")
-    {:ok, payload} = JSEX.decode(response.body)
-    assert payload["status"] == 200
-  end
-end
