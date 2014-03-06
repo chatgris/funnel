1779001dff5871120e06c9cc2315191f145f321d
chatgris <chatgri@gmail.com>
2014-03-06 18:47:28 +0100

    Fix typo on Events
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/event_stream_header.ex
+++ lib/event_stream_header.ex
@@ -1,9 +1,9 @@
 defmodule EventStreamHeader do
   @moduledoc """
-  This module set the response `Content-Type` Header to `text/events-stream`,
+  This module set the response `Content-Type` Header to `text/event-stream`,
   the ServerSent Events header.
   """
   def prepare(conn) do
-    conn.put_resp_header "Content-Type", "text/events-stream"
+    conn.put_resp_header "Content-Type", "text/event-stream"
   end
 end
