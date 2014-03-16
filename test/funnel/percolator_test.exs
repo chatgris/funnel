5c9f20ed58d07bd6013f6a5f23575ba5daf0ab1b
chatgris <jboyer@af83.com>
2014-03-16 15:10:48 +0100

    [Gardening] Sempler test
    Signed-off-by: chatgris <jboyer@af83.com>

--- test/funnel/percolator_test.exs
+++ test/funnel/percolator_test.exs
@@ -2,12 +2,7 @@ defmodule Funnel.PercolatorTest do
   use Funnel.TestCase, async: true
 
   test "percolator is alive" do
-    percolator = case Funnel.Percolator.start_link(nil) do
-      {:error, {:already_started, percolator}} ->
-        percolator
-      {:ok, percolator} ->
-        percolator
-    end
+    {:ok, percolator} = Funnel.Percolator.start_link(nil)
     assert Process.alive?(percolator)
   end
 end
