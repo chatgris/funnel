21b9a6eb39a8b18bf7e65059ea3a8d3860ea05b0
chatgris <jboyer@af83.com>
2014-05-08 18:13:00 +0200

    Add Funnel.Transport protocol for Elixir.Dynamo.Cowboy.Connection
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/funnel/transport.ex
+++ lib/funnel/transport.ex
@@ -2,3 +2,9 @@ defprotocol Funnel.Transport do
   @doc "Write a message"
   def write(receiver, message)
 end
+
+defimpl Funnel.Transport, for: Elixir.Dynamo.Cowboy.Connection do
+  def write(conn, %{:id => id, :item => item}) do
+    conn.chunk EventStreamMessage.to_message(id, item)
+  end
+end
