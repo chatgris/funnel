240f86f2080d6adbb4eecb32bbaf7972fc58e863
chatgris <jboyer@af83.com>
2014-05-08 18:25:09 +0200

    Filter transports on `:error`
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/funnel/transistor.ex
+++ lib/funnel/transistor.ex
@@ -62,8 +62,10 @@ defmodule Funnel.Transistor do
   Writes on each transports the matched document.
   """
   def handle_cast({:notify, id, item}, state) do
-    Enum.each(state.transports, fn(transport) -> write(transport, message(id, item)) end)
-    {:noreply, state }
+    transports = Enum.reduce(state.transports, [], fn(transport, transports) ->
+      filter_transports(transports, write(transport, message(id, item)))
+    end)
+    {:noreply, %TransistorState{state | transports: transports}}
   end
 
   @doc """
@@ -73,6 +75,14 @@ defmodule Funnel.Transistor do
   def handle_call({:add, transport, last_id}, _from, state) do
     transport = write_from_cache(transport, state.cache, last_id)
     {:reply, transport, %TransistorState{state | transports: [transport | state.transports]}}
+  end
+
+  defp filter_transports(transports, {:ok, transport}) do
+    [ transport | transports ]
+  end
+
+  defp filter_transports(transports, {:error, _}) do
+    transports
   end
 
   defp name(token) do
