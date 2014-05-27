5027477ce32ad4f101f07789fde077722951be90
chatgris <jboyer@af83.com>
2014-05-27 16:03:31 +0200

    Extract main API into Funnel module
    Signed-off-by: chatgris <jboyer@af83.com>

--- web/routers/river_router.ex
+++ web/routers/river_router.ex
@@ -4,9 +4,8 @@ defmodule RiverRouter do
   filter TokenFilter
 
   get "/" do
-    {:ok, _transistor} = Funnel.Transistors.add conn.assigns[:token]
     conn = conn.send_chunked(200)
-    conn = Funnel.Transistor.add(conn, conn.assigns[:token], conn.params[:last_id])
+    conn = Funnel.register(conn, conn.assigns[:token], conn.params[:last_id])
     await(conn, &on_wake_up(&1, &2))
   end
 
