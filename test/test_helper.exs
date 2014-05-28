9ef02df0d1a12359b7b932bae8c73def959fff9f
chatgris <chatgri@gmail.com>
2014-05-28 13:26:59 +0200

    Extract Es related assert into a module
    Signed-off-by: chatgris <jboyer@af83.com>

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -24,3 +24,41 @@ defimpl Funnel.Transport, for: Dynamo.Connection.Test do
     conn.chunk(item)
   end
 end
+
+defmodule Funnel.Es.Asserts do
+  import ExUnit.Assertions, only: [assert: 1, assert: 2]
+
+  def assert_query_creation(query, index \\ "funnel") do
+    {status, response} = Funnel.Es.register(index, "token", query)
+    {:ok, body} = JSEX.decode response
+    assert status == 201
+    body
+  end
+
+  def assert_query_update(query, uuid, index \\ "funnel") do
+    {status, response} = Funnel.Es.register(index, "token", uuid, query)
+    {:ok, body} = JSEX.decode response
+    assert status == 200
+    body
+  end
+
+  def assert_query_find(index) do
+    hash = HashDict.new
+    hash = Dict.put(hash, :index_id, index)
+    {status, response} = Funnel.Es.find("token", hash)
+    {:ok, response} = JSEX.decode response
+    assert status == 200
+    response
+  end
+
+  def assert_query_find do
+    {status, response} = Funnel.Es.find("token")
+    {:ok, response} = JSEX.decode response
+    assert status == 200
+    response
+  end
+
+  def assert_percolate(%{"_id" =>id, "_index" => _}, uuid) do
+    assert id == "token-#{uuid}"
+  end
+end
