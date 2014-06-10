1c8a1da45e7391b15135bfe72f3f507636203875
chatgris <jboyer@af83.com>
2014-06-10 21:14:18 +0200

    Give a bulk flavor to `Funnel.percolate`
    `Funnel.percolate` now accepts a json object, or a list of json objets.
    Because bulk api are so cool.
    
    Signed-off-by: chatgris <jboyer@af83.com>

--- test/funnel/percolator_test.exs
+++ test/funnel/percolator_test.exs
@@ -11,7 +11,7 @@ defmodule Funnel.PercolatorTest do
     token = Funnel.register(self)
     index_id = "percolator_index"
     uuid = assert_query_creation('{"query" : {"match" : {"message" : "elasticsearch"}}}', index_id, token)["query_id"]
-    message = '{"doc" : {"message" : "this new elasticsearch percolator feature is nice, borat style"}}'
+    message = "{\"doc\":{\"message\":\"this new elasticsearch percolator feature is nice, borat style\"}}"
 
     Funnel.Es.refresh
     Funnel.percolate(index_id, message)
@@ -27,10 +27,32 @@ defmodule Funnel.PercolatorTest do
     index_id = "percolator_index_multiple"
     uuid1 = assert_query_creation('{"query" : {"match" : {"message" : "thanks"}}}', index_id, token)["query_id"]
     uuid2 = assert_query_creation('{"query" : {"match" : {"message" : "fish"}}}', index_id, token)["query_id"]
-    message = '{"doc" : {"message" : "So long, and thanks for all the fish"}}'
+    message = "{\"doc\" : {\"message\":\"So long, and thanks for all the fish\"}}"
 
     Funnel.percolate(index_id, message)
 
+    assert_receive({:chunk, %{id: _, item: item}})
+    refute_receive({:chunk, %{id: _, item: _}})
+
+    {:ok, item} = JSEX.decode item
+    assert item["query_ids"] |> Enum.sort == [uuid1, uuid2] |> Enum.sort
+
+    Funnel.Es.unregister(index_id, token, uuid1)
+    Funnel.Es.unregister(index_id, token, uuid2)
+    Funnel.Es.unregister(index_id)
+  end
+
+  test "notify transistor with a message matching several queries, bulk edition" do
+    token = Funnel.register(self)
+    index_id = "percolator_index_multiple"
+    uuid1 = assert_query_creation('{"query" : {"match" : {"message" : "thanks"}}}', index_id, token)["query_id"]
+    uuid2 = assert_query_creation('{"query" : {"match" : {"message" : "fish"}}}', index_id, token)["query_id"]
+    message = "{\"doc\" : {\"message\":\"So long, and thanks for all the fish\"}}"
+    message2 = "{\"doc\":{\"message\":\"Say thanks to the fish\"}}"
+
+    Funnel.percolate(index_id, [message, message2])
+
+    assert_receive({:chunk, %{id: _, item: item}})
     assert_receive({:chunk, %{id: _, item: item}})
     refute_receive({:chunk, %{id: _, item: _}})
 
