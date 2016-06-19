07f8c708c3d659b66200a2060c70d9e8ef0d1733
chatgris <chatgri@gmail.com>
2016-06-19 15:34:48 +0200

    Upgrade dependencies
    Signed-off-by: chatgris <julien@praditus.com>

--- mix.exs
+++ mix.exs
@@ -19,11 +19,11 @@ defmodule Funnel.Mixfile do
   end
 
   defp deps do
-    [ { :httpoison,   "~> 0.6" },
-      { :poison,      "~> 1.5" },
-      { :uuid,        "~> 0.1.3" },
-      { :ex_doc,      "~> 0.5", only: [:dev] },
-      { :poolboy,     "~> 1.2" }
+    [ { :httpoison,   "~> 0.8" },
+      { :poison,      "~> 2.1" },
+      { :uuid,        "~> 1.1" },
+      { :ex_doc,      "~> 0.5", only: :dev },
+      { :poolboy,     "~> 1.5" }
     ]
   end
 
