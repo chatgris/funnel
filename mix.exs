0086c15b8ad7f5ea2c65a8435ac892136ae7a727
chatgris <chatgri@gmail.com>
2016-06-28 16:54:03 +0200

    Upgrade to Elixir 1.3
    Signed-off-by: chatgris <julien@praditus.com>

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Funnel.Mixfile do
       version: "0.4.1",
       compilers: [:elixir, :app],
       compile_path: "tmp/#{Mix.env}/funnel/ebin",
-      elixir: "~> 1.2.0",
+      elixir: "~> 1.2",
       description: description,
       package: package,
       consolidate_protocols: Mix.env == :prod,
